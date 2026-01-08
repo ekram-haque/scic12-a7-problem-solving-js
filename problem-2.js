ff4e5fbc1385a55a315b474bece5ebc97afc1f51
ekram-haque <dev.ekramul@gmail.com>
2026-01-08 16:52:26 +0600

    solve problem 2

---
+++ problem-2.js
@@ -0,0 +1,14 @@
+function countVowels(text) {
+  let count = 0;
+  let vowels = "aeiou";
+
+  for (let i = 0; i < text.length; i++) {
+    if (vowels.includes(text[i].toLowerCase())) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// console.log(countVowels("programming")); 
