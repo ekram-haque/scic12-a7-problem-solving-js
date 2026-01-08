21040b0ee4d009e56696be305f305a9a9b57a9c4
ekram-haque <dev.ekramul@gmail.com>
2026-01-08 16:49:27 +0600

    solve problem 1

---
+++ problem-1.js
@@ -0,0 +1,11 @@
+function reverseString(text) {
+  let result = "";
+
+  for (let i = 0; i < text.length; i++) {
+    result = text[i] + result;
+  }
+
+  return result;
+}
+
+// console.log(reverseString("hello"));
